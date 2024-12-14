906748ea029388c35f9c7ff1938de20de1622d9d
Joshua Winters-Brown <joshuawintersbrown@gamil.com>
2024-12-13 19:25:44 -0500

    Functioning Combat. Not really what It should be but works for demo purposes

---
+++ Sceens/Combat/Enemy.gd
@@ -0,0 +1,42 @@
+extends Sprite2D
+
+
+var is_shaking = false  # To prevent multiple shake calls
+var shake_intensity = 0  # How far the object moves when shaking
+var wobble_intensity = 5  # How much the object rotates
+var shake_duration = 0.3  # Duration of the effect
+var shake_timer = 0.0  # Internal timer
+@onready var starting_position = Vector2(2, 59) # Hard Coded.... Cant figure out how to get these in script
+
+func _ready():
+	pass  # Called when the node is added to the scene
+
+func _process(delta):
+	if is_shaking:
+		shake_timer += delta
+		if shake_timer > shake_duration:
+			is_shaking = false
+			set_position(starting_position)  # Reset position to original
+			rotation = 0  # Reset rotation
+			set_process(false)  # Disable processing
+		else:
+			apply_shake_and_wobble()
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		if not is_shaking:
+			start_shake_and_wobble()
+
+func start_shake_and_wobble():
+	is_shaking = true
+	shake_timer = 0.0  # Reset the timer
+	set_process(true)  # Enable processing
+
+func apply_shake_and_wobble():
+	# Random shake offset
+	var shake_x = randf_range(-shake_intensity, shake_intensity)
+	var shake_y = randf_range(-shake_intensity, shake_intensity)
+	position = Vector2(shake_x, shake_y)
+
+	# Random wobble rotation
+	rotation = deg_to_rad(randf_range(-wobble_intensity, wobble_intensity))
