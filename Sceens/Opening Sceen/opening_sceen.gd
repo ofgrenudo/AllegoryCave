2968790efc2cc7bd2ede4092b53e2f917ff99df3
Joshua Winters-Brown <joshuawintersbrown@gmail.com>
2024-12-17 00:09:47 -0500

    refactor: overhaul combat with dynamic cards and deck building

--- Sceens/Opening Sceen/opening_sceen.gd
+++ Sceens/Opening Sceen/opening_sceen.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var navigation = preload("res://Sceens/Navigation/Navigation.tscn")
+@onready var cards = get_node("Room/Cards")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,5 +11,13 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-func _on_proceed_button_pressed() -> void:
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("select"):
 		get_tree().change_scene_to_packed(navigation)
+
+func _on_area_2d_mouse_entered() -> void:
+	cards.scale += Vector2(0.05, 0.05)
+
+
+func _on_area_2d_mouse_exited() -> void:
+	cards.scale -= Vector2(0.05, 0.05)
