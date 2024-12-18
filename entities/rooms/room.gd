1eb2911ac2053b3f9eada8dbc3de1a584b57ac19
Joshua Winters-Brown <joshuawintersbrown@gamil.com>
2024-12-18 15:50:24 -0500

    Fixed #13

--- entities/rooms/room.gd
+++ entities/rooms/room.gd
@@ -1,11 +1,29 @@
 class_name Room extends Control
 
+enum RoomType {
+	FORWARD_LEFT,
+	FORWARD_RIGHT,
+	INTRO_WAKE_UP,
+	LEFT,
+	LEFT_OR_RIGHT_ONE,
+	LEFT_OR_RIGHT_TWO,
+	RIGHT,
+	ROOM,
+	TITLE_CARD,
+}
+
+@export var selected_room: RoomType = RoomType.FORWARD_LEFT
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	load_selected_room()
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func load_selected_room():
 	pass
